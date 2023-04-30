406c75d017f7bb5ed3d7fed1bc4b77acc592b603
vburduja11 <vburduja11@gmail.com>
2023-04-30 09:47:50 -0500

    Objects

--- 07_Object.js
+++ 07_Object.js
@@ -5,4 +5,19 @@ let person = {
 
 console.log(typeof person);
 console.log(person);
+console.log(person.lastName);
+
+
+let adress = {
+    'building' : 5555,
+    street : 'Keowee',
+    state : 'Kentuky'
+}
+console.log(adress);
+console.log(adress['building']);
+console.log(adress['street']);
+
+// add new property to object
+person.age = 30;
+console.log(person);
 
